1134114e88aedf6def65bfb8fc0fe39c4116b4c0
snezanazivaljevic <129274447+snezanazivaljevic@users.noreply.github.com>
2023-03-29 12:08:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie ')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega3 & Bluberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie ')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
